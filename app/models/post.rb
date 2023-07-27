6725ea987763c1399094d4b5f550dce4771b2677
m720 <moatasem.720@gmail.com>
2023-07-27 04:59:30 +0300

    adds validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, length: { minimum: 2, maximum: 255 }, presence: true
-
+  validates :body, length: { minimum: 8, maximum: 2500 }, presence: true
 end
