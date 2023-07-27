98b094b17a7f9452195fa0227f59472274b48f14
m720 <moatasem.720@gmail.com>
2023-07-27 05:38:58 +0300

    adding more CRUDs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  # TODO: add edit history
   validates :title, length: { minimum: 2, maximum: 255 }, presence: true
   validates :body, length: { minimum: 8, maximum: 2500 }, presence: true
+
+  scope :by_user_id, ->(user_id) { where(user_id)}
 end
